01cc6c776615dc4c4ad270ba461d8ff3f80c4bd4
Sumit <gurjar.sumitu4500@.com>
2024-04-11 12:14:59 +0530

    authentication middleware added to single point

--- cart-service/src/routes/cartRoutes.js
+++ cart-service/src/routes/cartRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const cartController = require('../controllers/cartController');
-const { authMiddleware } = require('../middleware/authMiddleware');
 
-router.post('/create', authMiddleware, cartController.createCart);
+router.post('/create', cartController.createCart);
 
-router.get('/get', authMiddleware, cartController.getCart);
+router.get('/get', cartController.getCart);
 
-router.post('/addToCart', authMiddleware, cartController.addToCart);
+router.post('/addToCart', cartController.addToCart);
 
-router.post('/delete/:cartId', authMiddleware, cartController.deleteCart);
+router.post('/delete/:cartId', cartController.deleteCart);
 
 module.exports = router;
